48e7d7354b621445b41a3eb70ef78924d5d124e9
Michael S Collins <mercutio.viz@gmail.com>
2020-05-23 23:09:57 +0000

    update vents.sh

--- vnets.sh
+++ vnets.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-vnet_json=`az network vnet list --query "[?location=='eastus2']" | json_pp`
+
+if [ -z "$1" ]
+then
+    echo "Please supply location as arg, ex: ./list-rgs eastus2"
+    exit 1
+fi
+location="$1"
+
+vnet_json=`az network vnet list --query "[?location=='${location}']" | json_pp`
 vnetlist=`echo "${vnet_json}" | jq -r '.[] | .name'`
 vnet_count=$((`echo ${vnet_json} | jq '. | length'`-1))
 #'
