1431c8b6b5f01024d0fd7f4f4cff4980a2ad2725
Michael S Collins <mercutio.viz@gmail.com>
2020-09-17 18:32:37 +0000

    Add more logic to vnets.sh

--- vnets.sh
+++ vnets.sh
@@ -12,10 +12,13 @@ vnetlist=`echo "${vnet_json}" | jq -r '.[] | .name'`
 vnet_count=$((`echo ${vnet_json} | jq '. | length'`-1))
 #'
 ##
-myvnet='VNET-E2US'
+myvnet='LabNet1'
 for I in `seq 0 "${vnet_count}"`; do
   this_vnet=`echo "$vnet_json" | jq -r --arg 'I' $I '.[$I | tonumber] | .name'`
   if [[ "${this_vnet}" == "${myvnet}" ]]; then
+    echo -n "** ${I}: "
+    echo "${this_vnet}"
+  else
     echo -n "${I}: "
     echo "${this_vnet}"
   fi
